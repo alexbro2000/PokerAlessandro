24f3fd81fc6f2b39377cb4d470716b32cc14c1ec
alexbro2000 <acolon@browning.edu>
2025-11-13 14:18:41 +0000

    Deck.java

--- Deck.java
+++ Deck.java
@@ -1,11 +1,46 @@
 // Deck.java
 public class Deck{
+private Card card1;
 
-//public Card drawCard(){
-   // Math.random();
-   // Math.random('H','D','C','S');
-   // return new Card(value, suit);
-}
-}
+    public Card getRandomCard() {
+        // Generate a number from 1 to 13.
+        // Math.random() gives a random number from 0 to 1.
+        int number = (int) Math.floor((Math.random() * 13)) + 1;
 
+        // Generate a number from 0 to 3.
+        int suitNumber = (int) Math.floor((Math.random() * 3));
+        char suit;
+        switch (suitNumber) {
+            case 0:
+                suit = 'h';
+                break;
+            case 1:
+                suit = 's';
+                break;
+            case 2:
+                suit = 'd';
+                break;
+            case 3:
+                suit = 'c';
+                break;
+            default:
+                throw new Error("wtf did you do");
+        }
+
+        // this is equivalent to:
+        if (suitNumber == 0) {
+            suit = 'h';
+        } else if (suitNumber == 1) {
+            // etc etc.
+        } else {
+            throw new Error("wtf did you do");
+        }
+
+        return new Card(number, suit);
+    }
+
+    public Deck() {
+        this.card1 = getRandomCard();
+    }
+}
 
