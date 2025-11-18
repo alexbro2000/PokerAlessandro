c6a145522a6c2bb2363183cc5c735af3314047b4
alexbro2000 <acolon@browning.edu>
2025-11-18 15:24:09 +0000

    made player class, might work

---
+++ Player.java
@@ -0,0 +1,48 @@
+public class Player{
+ public static void Player(String[] args) { 
+    private int money;
+    private String name;
+
+    public Player(String name, int money) {
+        this.name = name;
+        this.money = money;
+
+    }
+   
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void takeMoney(int amount) {
+        this.money -= amount;
+    }
+
+    public void giveMoney(int amount) {
+        this.money += amount;
+    }
+
+    public void getCard() {
+        char x = 'S';
+        int y = 10;
+        int value = y;
+        char suit = x;
+
+        System.out.println("New card: " + value + " of " + suit);
+    }
+   
+    
+
+}
